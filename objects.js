043cc7b8ee2ef1dc3c90da791d87699bf635df54
loretta_okoye@hotmail.com <loretta_okoye@hotmail.com>
2018-05-24 09:43:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {LilBoosie: "Wipe Me Down"};
 
 function updatePlaylist(playlist,artist,song){
-  playlist[artist]=
-  return playlist[artist]=song
+  playlist[artist]=song
+  return playlist
 }
